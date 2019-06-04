27a1ca924c805638cf0c4c009837b79818f0a0d7
Marta Sajdak <crappylime@gmail.com>
2019-06-04 12:56:40 +0200

    Add auth, guard middlewares and error handler

--- express/server.js
+++ express/server.js
@@ -2,16 +2,34 @@ const express = require('express');
 const app = express();
 
 app.get('/',
-    function (req, res, next) {
+
+    function loggerMidlleware(req, res, next) {
         console.log(req.url);
         next();
     },
 
-    function (req, res) {
+    function authMidlleware(req, res, next) {
+        console.log('auth', req.query.name);
+        setTimeout(() => {
+            req.user = req.query.name;
+            next();
+        }, 1000)
+    },
+
+    function guardMiddleware(req, res, next) {
+        req.user ? next() : next('forbidden');
+    },
+
+    function requestHandler(req, res) {
         // res.send('Hello')
         res.json({
+            user: req.user,
             message: 'Hello'
         })
+    },
+
+    function errorHandler(err, req, res, next) {
+        res.json({ status: 500, err })
     }
 )
 
