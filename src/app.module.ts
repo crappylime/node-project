9c219463f78a20dbf93da5063d332b6020bea414
Marta Sajdak <crappylime@gmail.com>
2019-06-05 13:32:32 +0200

    Generate user module with controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommentsModule } from './comments/comments.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [CommentsModule],
+  imports: [CommentsModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
