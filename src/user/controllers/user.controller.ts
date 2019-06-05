da6f05a2400d8d5f800cc9b1a67c562274912719
Marta Sajdak <crappylime@gmail.com>
2019-06-05 14:12:33 +0200

    Add register endpoint to post a user

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, InternalServerErrorException, Post } from '@nestjs/common';
+import { ApiCreatedResponse } from '@nestjs/swagger';
+import { UserRegisterRequestDto, UserRegisterResponseDto } from '../dto';
+import { UserService } from '../services/user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+    constructor(private userService: UserService) { }
+
+    @Post('register')
+    @ApiCreatedResponse({ type: UserRegisterResponseDto })
+    async register(@Body() data: UserRegisterRequestDto): Promise<UserRegisterResponseDto> {
+        try {
+            const user = await this.userService.create(data);
+            return {
+                user,
+            };
+        } catch (error) {
+            throw new InternalServerErrorException('error', error);
+        }
+    }
+}
