39df3ad84930f3c380ceb2157ced26b5b6233c67
Marta Sajdak <crappylime@gmail.com>
2019-06-05 10:18:16 +0200

    Define comment model and dtos

---
+++ src/dto/comment.dto.ts
@@ -0,0 +1,19 @@
+import { CommentModel } from './../models';
+
+export class GetCommentsRequestDto {
+    search: string;
+    pageIndex: number;
+    pageSize: number;
+}
+
+export class GetCommentsResponseDto {
+    pageIndex: number;
+    pageSize: number;
+    data: CommentModel[];
+    query: GetCommentsRequestDto;
+}
+
+export class GetCommentResponseDto {
+    total: number;
+    data: CommentModel;
+}
