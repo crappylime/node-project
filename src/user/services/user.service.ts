2e3e1857c3146f25dd3e4fb13ab2f42b838e6ca6
Marta Sajdak <crappylime@gmail.com>
2019-06-05 14:14:37 +0200

    Handle async creating a user without the db

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { UserRegisterRequestDto } from '../dto';
+import { UserModel } from '../models';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    users: UserModel[] = [];
+
+    async create(data: UserRegisterRequestDto): Promise<UserModel> {
+        const user: UserModel = {
+            id: this.users.length + 1,
+            email: data.email,
+            name: data.name,
+            password: data.password,
+            roles: [],
+        };
+        this.users.push(user);
+        return user;
+    }
+}
