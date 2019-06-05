0b99955dd7996fc78a51ca1f2e41d141815ca7dc
Marta Sajdak <crappylime@gmail.com>
2019-06-05 11:21:14 +0200

    Configure Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
@@ -11,6 +12,19 @@ async function bootstrap() {
     new ExpressAdapter(expressApp),
   );
   app.setGlobalPrefix('api');
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest API Example')
+    .setDescription('Przykładowy projekt w Node.js i Typescript')
+    .setVersion('1.0')
+    .addTag('user')
+    // .addBearerAuth(config.TOKEN_HEADER_NAME, 'header')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
+
 }
 bootstrap();
