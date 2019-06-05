c2a5e84d992b9016349bc4a84ba15c8bf84721e7
Marta Sajdak <crappylime@gmail.com>
2019-06-05 14:15:56 +0200

    Add swagger annotations to user model and dto

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,11 +1,16 @@
+import { ApiModelProperty } from '@nestjs/swagger';
 import { UserModel } from '../models';
 
 export class UserRegisterRequestDto {
+    @ApiModelProperty()
     name: string;
+    @ApiModelProperty()
     email: string;
+    @ApiModelProperty()
     password: string;
 }
 
 export class UserRegisterResponseDto {
+    @ApiModelProperty()
     user: UserModel;
 }
--- src/user/models/user.model.ts
+++ src/user/models/user.model.ts
@@ -1,11 +1,18 @@
+import { ApiModelProperty, ApiModelPropertyOptional } from '@nestjs/swagger';
+
 export enum UserRole {
     ADMIN = 'admin',
     ROOT = 'root',
 }
 export class UserModel {
+    @ApiModelPropertyOptional()
     id?: number;
+    @ApiModelProperty()
     name: string;
+    @ApiModelPropertyOptional()
     email?: string;
+    @ApiModelPropertyOptional()
     password?: string;
+    @ApiModelPropertyOptional({ enum: UserRole, isArray: true })
     roles?: UserRole[];
 }
