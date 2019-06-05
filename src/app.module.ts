7b3bb51eb0180ff4c71b50d333556d71a6f4e58a
Marta Sajdak <crappylime@gmail.com>
2019-06-05 17:04:08 +0200

    Fix and export config service from its module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CommentsModule } from './comments/comments.module';
 import { ConfigModule } from './config/config.module';
-import { ConfigService } from './config/config.service';
 import { UserModule } from './user/user.module';
 
 @Module({
   imports: [CommentsModule, UserModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
-  exports: [ConfigService],
 })
 export class AppModule { }
