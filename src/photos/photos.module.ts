8ed036a93954588ad105c91c15045792dfd2881b
Marta Sajdak <crappylime@gmail.com>
2019-06-07 10:35:52 +0200

    Import library that handles files

--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { MulterModule } from '@nestjs/platform-express';
+import { ConfigService } from '../config';
 import { PhotosController } from './controller/photos.controller';
 import { PhotosService } from './services/photos.service';
 
 @Module({
+  imports: [
+    MulterModule.registerAsync({
+      useFactory: async (config: ConfigService) => ({
+        dest: config.STORAGE_TMP,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [PhotosController],
   providers: [PhotosService]
 })
-export class PhotosModule {}
+export class PhotosModule { }
