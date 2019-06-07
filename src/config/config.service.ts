5837f72db0567979dbe6fabda51a519da0f8a2d3
Marta Sajdak <crappylime@gmail.com>
2019-06-07 10:30:36 +0200

    Resolve photos paths, make fields readonly

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { resolve } from 'path';
 
 @Injectable()
 export class ConfigService {
-    JWT_SECRET = 'jwt-secret';
-    TOKEN_HEADER_NAME = 'api_token';
+    readonly JWT_SECRET = 'jwt-secret';
+    readonly TOKEN_HEADER_NAME = 'api_token';
+
+    readonly STORAGE_TMP = resolve(__dirname, '../../storage/tmp');
+    readonly STORAGE_PHOTOS = resolve(__dirname, '../../assets/photos');
+    readonly PHOTOS_BASE_PATH = '/photos';
 }
