4125a525d7d2e95a8e55d49bba65e67618f6352f
Marta Sajdak <crappylime@gmail.com>
2019-06-07 11:31:12 +0200

    Import orm with photo entities

--- src/photos/photos.module.ts
+++ src/photos/photos.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigService } from '../config';
 import { PhotosController } from './controller/photos.controller';
+import * as entities from './entities';
 import { PhotosService } from './services/photos.service';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature(Object.values(entities)),
     MulterModule.registerAsync({
       useFactory: async (config: ConfigService) => ({
         dest: config.STORAGE_TMP,
