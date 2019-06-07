54722524246d4ff03cf7b3413c7bcde29c6ee42e
Marta Sajdak <crappylime@gmail.com>
2019-06-07 14:29:36 +0200

    Login users to chat, connect/disconnect them

--- src/gateways/chat.gateway.ts
+++ src/gateways/chat.gateway.ts
@@ -5,6 +5,21 @@ import { Server, Socket } from 'socket.io';
 @WebSocketGateway({ namespace: '/chat' })
 export class ChatGateway implements OnGatewayInit {
 
+  private users = {};
+
+  handleConnection(client: any, ...args: any[]) {
+    this.users[client.id] = {
+      client,
+      name: '',
+    };
+    this.sendUsers();
+  }
+
+  handleDisconnect(client: any) {
+    delete this.users[client.id];
+    this.sendUsers();
+  }
+
   @WebSocketServer() wss: Server;
 
   private logger: Logger = new Logger('ChatGateway');
@@ -16,5 +31,15 @@ export class ChatGateway implements OnGatewayInit {
   @SubscribeMessage('chatToServer')
   handleMessage(client: Socket, message: { sender: string, message: string }) {
     this.wss.emit('chatToClient', message);
+  }
+
+  @SubscribeMessage('login')
+  handleLogin(client: Socket, message: { sender: string, message: string }) {
+    this.users[client.id].name = message.sender;
+    this.sendUsers();
+  }
+
+  sendUsers() {
+    this.wss.emit('users', Object.values(this.users).map((v: any) => v.name).filter(u => !!u));
   }
 }
