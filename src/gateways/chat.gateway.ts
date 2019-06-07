500e63dd4416ed01203f9dd76bd6bf617885edfd
Marta Sajdak <crappylime@gmail.com>
2019-06-07 13:57:36 +0200

    Implement basic chat; emit msg to all clients

--- src/gateways/chat.gateway.ts
+++ src/gateways/chat.gateway.ts
@@ -1,9 +1,20 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway()
-export class ChatGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+@WebSocketGateway({ namespace: '/chat' })
+export class ChatGateway implements OnGatewayInit {
+
+  @WebSocketServer() wss: Server;
+
+  private logger: Logger = new Logger('ChatGateway');
+
+  afterInit(server: any) {
+    this.logger.log('Initialized!');
+  }
+
+  @SubscribeMessage('chatToServer')
+  handleMessage(client: Socket, message: { sender: string, message: string }) {
+    this.wss.emit('chatToClient', message);
   }
 }
