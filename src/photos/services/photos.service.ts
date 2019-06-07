de73135e44fdc11476d7c66621c31503eb0816a9
Marta Sajdak <crappylime@gmail.com>
2019-06-07 10:45:46 +0200

    Save file locally

--- src/photos/services/photos.service.ts
+++ src/photos/services/photos.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { rename } from 'fs';
+import { extname, join } from 'path';
+import { promisify } from 'util';
+const renameAsync = promisify(rename);
+import * as crypto from 'crypto';
+import { ConfigService } from '../../config';
 
 @Injectable()
-export class PhotosService {}
+export class PhotosService {
+    constructor(private config: ConfigService) { }
+    async create(file) {
+        // TODO validate is photo
+        const fileName = crypto
+            .createHash('md5')
+            .update(file.path)
+            .digest('hex') + extname(file.originalname).toLowerCase();
+        await renameAsync(file.path, join(this.config.STORAGE_PHOTOS, fileName));
+        return {
+            fileName,
+        };
+    }
+}
