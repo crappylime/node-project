904ce551a2dd94a4dd58d362244ce6c9571a31d5
Marta Sajdak <crappylime@gmail.com>
2019-06-07 11:24:39 +0200

    Resolve path to the db

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -9,4 +9,7 @@ export class ConfigService {
     readonly STORAGE_TMP = resolve(__dirname, '../../storage/tmp');
     readonly STORAGE_PHOTOS = resolve(__dirname, '../../assets/photos');
     readonly PHOTOS_BASE_PATH = '/photos';
+
+    readonly DB_NAME = resolve(__dirname, '../../storage/databases/db.sql');
+
 }
