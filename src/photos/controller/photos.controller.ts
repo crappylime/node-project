e8c2c07afbb0bdcf853b970ab360956643ca23dd
Marta Sajdak <crappylime@gmail.com>
2019-06-07 10:44:40 +0200

    barrel new directories, send post with file

--- src/photos/controller/photos.controller.ts
+++ src/photos/controller/photos.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { ApiConsumes, ApiImplicitFile } from '@nestjs/swagger';
+import { PhotosService } from '../services';
 
 @Controller('photos')
-export class PhotosController {}
+export class PhotosController {
+
+    constructor(private photosService: PhotosService) { }
+
+    @ApiConsumes('multipart/form-data')
+    @ApiImplicitFile({ name: 'file', required: true, description: 'Upload user avatar' })
+    @Post('upload-user-avatar')
+    @UseInterceptors(FileInterceptor('file'))
+    async uploadFile(@UploadedFile() file, @Body() body) {
+        const avatar = await this.photosService.create(file);
+        return { avatar, file, body };
+    }
+}
